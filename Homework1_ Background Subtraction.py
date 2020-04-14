ca48b712d502e759260f0a453e70516a2cb82ff3
conrad2001 <conrad2001@users.noreply.github.com>
2020-04-13 22:20:20 -0700

    renamed video and audio files

--- Homework1_ Background Subtraction.py
+++ Homework1_ Background Subtraction.py
@@ -19,16 +19,14 @@ def part1():
     image = np.array(image, dtype=np.uint8)
     cv2.imshow('000000.jpg', image)
     # Print the shape of the Numpy array containing the image -- what do the sizes of the dimensions mean?
-    #print(np.shape(image))
+    print(np.shape(image))
     # Print the image itself -- what do these numbers mean?
     print(image)
     # Convert the image to grayscale using cv2.cvtColor() and show it.
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow('grayscale', gray)
     # Save the grayscale image to a PNG using cv2.imwrite().
-    directory = r'C:\Users\User01\PycharmProjects\CPE428\gray'
-    os.chdir(directory)     # change to specified directory
-    cv2.imwrite('0.png', gray)
+    cv2.imwrite('part1.png', gray)
     cv2.waitKey(3000)
 
 def part2():
@@ -42,16 +40,13 @@ def part2():
     # Show each frame of the video
     i = 0
     grays = []
-    directory = r'C:\Users\User01\PycharmProjects\CPE428\part2'
-    os.chdir(directory)
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         window_name = 'image' + str(i) + '.jpg'
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow(window_name, gray)     # Show each frame of the video.
-        cv2.imwrite("%02d" % i + '.jpg', gray)
+        cv2.imshow(window_name, gray)     # Show each frame of the video.
         grays.append(np.array(gray))
         i += 1
     # calculate the mean frame
@@ -59,9 +54,7 @@ def part2():
     # Show the average image, what we will call the "background" image.  The cars have disappeared!  Why?
     cv2.imshow('average frame', mean)
     # Save the background image to a PNG.
-    directory = r'C:\Users\User01\PycharmProjects\CPE428\gray'
-    os.chdir(directory)
-    cv2.imwrite('1.png', mean)
+    cv2.imwrite('part2.png', mean)
     cv2.waitKey(1000)
     cap.release()
     cv2.destroyAllWindows()
@@ -73,10 +66,12 @@ def part3():
     """
 
     # Load the images from parts I and II.
-    image0 = cv2.imread(r'gray\0.png', cv2.IMREAD_GRAYSCALE)
-    image1 = cv2.imread(r'gray\1.png', cv2.IMREAD_GRAYSCALE)
+    image0 = cv2.imread(r'part1.png', cv2.IMREAD_GRAYSCALE)
+    image1 = cv2.imread(r'part2.png', cv2.IMREAD_GRAYSCALE)
     # calculate the absolute difference btw image 1 and 2
     abs_diff = cv2.absdiff(image0, image1)
+    cv2.imshow('abs diff', abs_diff)
+    cv2.imwrite('abs diff.png', abs_diff)
     threshold = 30
     # Threshold the absolute difference image to obtain a binary mask corresponding to the foreground pixels
     Ret, threshold_frame = cv2.threshold(abs_diff, threshold, 255, cv2.THRESH_BINARY)
@@ -84,13 +79,14 @@ def part3():
     # Threshold the absolute difference image using Otsu's method
     Ret_Otsu, Otsu = cv2.threshold(abs_diff, threshold, 255, cv2.THRESH_OTSU)
     cv2.imshow("Otsu's threshold" + str(threshold), Otsu)
+    cv2.imwrite("part3.png", Otsu)
     cv2.waitKey(10000)
     # How well does each technique work?  What could be improved about the output?
 
 # Bonus:
 
 
-def movie():
+def bonus1():
     """Run the thresholding technique on each frame of the video and show the result as a movie.
     """
     cap = cv2.VideoCapture('frames/%06d.jpg')
@@ -107,15 +103,13 @@ def movie():
         Otsu = cv2.cvtColor(Otsu, cv2.COLOR_GRAY2BGR)
         size = (width, height)
         Otsu_arr.append(Otsu)
-    directory = r'C:\Users\User01\PycharmProjects\CPE428\movie'
-    os.chdir(directory)
-    video = cv2.VideoWriter(filename="Otsu.avi", fourcc=cv2.VideoWriter_fourcc('M', 'P', 'E', 'G'), fps=5, frameSize=size)
+    video = cv2.VideoWriter(filename="bonus1.avi", fourcc=cv2.VideoWriter_fourcc('M', 'P', 'E', 'G'), fps=5, frameSize=size)
     for Otsu in Otsu_arr:
         video.write(Otsu)
     video.release()
 
 
-def bounding():
+def bonus2():
     """Detect a bounding box around each car and show the result as a movie.
     """
     cap = cv2.VideoCapture('frames/%06d.jpg')
@@ -136,9 +130,8 @@ def bounding():
                 height, width = np.shape(Otsu)
                 bound_img = np.copy(Otsu)
                 bound_img = cv2.cvtColor(bound_img, cv2.COLOR_GRAY2BGR)
-                Otsu = cv2.blur(Otsu, ksize=(3,2))
-                contours, hierarchy = cv2.findContours(Otsu, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
+                blurr = cv2.blur(Otsu, ksize=(3, 2))
+                contours, hierarchy = cv2.findContours(blurr, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
                 for cidx, cnt in enumerate(contours):
                     (x, y, w, h) = cv2.boundingRect(cnt)
                     # Draw a rectangle with blue line borders of thickness of 1 px
@@ -147,16 +140,15 @@ def bounding():
                 Otsu_arr.append(bound_img)
                 size = (width, height)
             previous = current
-    directory = r'C:\Users\User01\PycharmProjects\CPE428\movie'
-    os.chdir(directory)
-    video = cv2.VideoWriter(filename="bounding.avi", fourcc=cv2.VideoWriter_fourcc('M', 'P', 'E', 'G'), fps=15, frameSize=size)
+    video = cv2.VideoWriter(filename="bonus2.avi", fourcc=cv2.VideoWriter_fourcc('M', 'P', 'E', 'G'), fps=15, frameSize=size)
     for Otsu in Otsu_arr:
         video.write(Otsu)
     video.release()
 
 
 def main():
-    bounding()
+    bonus1()
+    bonus2()
 
 
 if __name__ == '__main__':
