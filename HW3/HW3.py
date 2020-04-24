242c462c7a14f1b0e83020e54e154e0fb76419a3
conrad2001 <conrad2001@users.noreply.github.com>
2020-04-24 02:49:55 -0700

    need to finish bonus part

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -34,20 +34,19 @@ def HW3():
     a[k][k] = 1     # generate all pass filer (one in the middle)
     h = a-g
 
-
-    cv2.imshow('h', h)
     # Step 4: Filter the dog image with the low-pass kernel.  Filter the cat image with the high-pass kernel
     dog = cv2.filter2D(src=dog, ddepth=-1, kernel=g)
     cat = cv2.filter2D(src=cat, ddepth=-1, kernel=h)
     cv2.imshow('dog', dog)
     cv2.imshow('cat', cat)
 
-
     # Step 5: Add the low-passed dog and high-passed cat together to produce the hybrid image.
-    hybrid = ((dog+cat)*255).astype('uint8')
-    cv2.imshow('hybrid', hybrid)
-    cv2.waitKey(8000)
+    hybrid = dog+cat
+    cv2.imshow('hybrid_out.png', hybrid)
+    cv2.waitKey(5000)
 
+    # bonus
+    # Make your own custom hybrid image using two images that you find / capture
 
 
 if __name__ == '__main__':
